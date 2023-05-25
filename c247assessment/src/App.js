17b2e2971018dde27169a504ad8b30f96115b3e9
Jamie <jamie@c247.it>
2023-05-25 15:49:31 +0200

    Empty react application, candidates should edit App.js to include their solution.

---
+++ c247assessment/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'; /* Change the styling as you like */
+
+function App() {
+    /* Come up with a way to fetch the json data from https://jamiec247.github.io/data.json and store it */
+
+    return (
+        <div className="App">
+            {/* Add your method for rendering the fetched data here */ }
+            <p>Write your solution in src/App.js.</p>
+        </div>
+    );
+}
+
+export default App;
